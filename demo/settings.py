9f7f9e252b7cec5efd57879a2c0055b768019b77
eingaruca <eldo16@gmail.com>
2024-04-01 09:37:31 +0200

    feat: Modifying Dockerfile & manifest.yaml

--- demo/settings.py
+++ demo/settings.py
@@ -14,9 +14,9 @@ env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = []
 
 
 # Application definition
